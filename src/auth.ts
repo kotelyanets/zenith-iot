a33b79b8589c2453d075c19f7bb22f3cc6369884
kotelyanets <andrekotelyanets@proton.me>
2026-02-11 00:56:12 +0000

    fix: export handlers from NextAuth for API route

--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,7 @@ import { prisma } from '@/lib/prisma';
 import bcrypt from 'bcryptjs';
 import Google from 'next-auth/providers/google';
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [
         Credentials({
